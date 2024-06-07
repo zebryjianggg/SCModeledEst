86a45a3c62c0139c6e68436ca4b0c7fdf14b823a
zebryjianggg <zheyu0521@gmail.com>
2024-06-07 01:53:52 -0700

    step 2 using saspy started - testing stage

---
+++ saspy_config.py
@@ -0,0 +1,11 @@
+# saspy_config.py
+
+SAS_config_names = ['oda']
+
+oda = {
+    'java': '/usr/bin/java',  # Path to the Java executable
+    'iomhost': 'odaws01-usw2.oda.sas.com',  # Host address for SAS OnDemand for Academics
+    'iomport': 8591,  # Default IOM port
+    'authkey': 'oda',  # Authentication key for SAS OnDemand
+    'encoding': 'utf-8'
+}
